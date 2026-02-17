9fdeb13e35f485bd3873a2732a4118bb1377f07d
YOUNG-RAJKUMAR <realrajkumarbitalu@gmail.com>
2026-02-17 12:10:56 +0545

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ counter.innerText = '0';
 const updateCounter = () => {
 const target = +counter.getAttribute('data-target');
 const c = +counter.innerText;
-
 const increment = target / 200;
 
 if(c < target){
