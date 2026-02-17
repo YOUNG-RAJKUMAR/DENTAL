9d723bd7d12294e9afab3c8ade96b81178f0906a
YOUNG-RAJKUMAR <realrajkumarbitalu@gmail.com>
2026-02-17 12:08:38 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,14 @@
-// Navbar color change on scroll
-window.addEventListener("scroll", function(){
-const header = document.getElementById("header");
-header.classList.toggle("scrolled", window.scrollY > 50);
-});
-
-// Scroll Reveal Animation
-function reveal(){
-const reveals = document.querySelectorAll(".reveal");
-for(let i=0;i<reveals.length;i++){
-let windowHeight = window.innerHeight;
-let elementTop = reveals[i].getBoundingClientRect().top;
-let elementVisible = 100;
-if(elementTop < windowHeight - elementVisible){
-reveals[i].classList.add("active");
-}
-}
-}
-window.addEventListener("scroll", reveal);
-
-// Counter Animation
 const counters = document.querySelectorAll(".counter");
+
 counters.forEach(counter => {
 counter.innerText = '0';
+
 const updateCounter = () => {
 const target = +counter.getAttribute('data-target');
 const c = +counter.innerText;
+
 const increment = target / 200;
+
 if(c < target){
 counter.innerText = `${Math.ceil(c + increment)}`;
 setTimeout(updateCounter, 10);
@@ -33,5 +16,6 @@ setTimeout(updateCounter, 10);
 counter.innerText = target;
 }
 };
+
 updateCounter();
 });
